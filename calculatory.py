0ea952adabb6005ae93ec6d95f2214409a72bef8
Marshawn Mcleod <marshmcl++gitrepo@amazon.com>
2022-11-17 14:47:24 -0500

    New calc app

---
+++ calculatory.py
@@ -0,0 +1,15 @@
+x = input("What's x?")
+y = input("What's y?")
+
+#Convert the retrun value into an int that use the + operator
+# z = x + y will concatenate the two str to give an incorrect value
+z = int(x) + int(y)
+
+print(z)
+
+## This is a more elegant way of sloving our problem. We convert the input directly into an int
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+print(x + y)
+
